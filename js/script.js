df1cce448cbbcf6e060029925cb854ef0c0bffde
Steven-Roduta <stevenroduta00@gmail.com>
2025-01-09 22:53:28 +0100

    add images

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const graphic = [
+    {
+      Text: "Some content for this column.",
+      img: "img/boat-ph.png"
+    },
+    {
+      Text: "Some content for this column.",
+      img: "img/foggy.jpg"
+    },
+    {
+      Text: "Some content for this column.",
+      img: "img/landscape-mountain.jpg"
+    },
+    {
+      Text: "Some content for this column.",
+      img: "img/pose-ph.jpg"
+    },
+    {
+      Text: "Some content for this column.",
+      img: "img/sunset-ph.jpg"
+    },
+    {
+      Text: "Some content for this column.",
+      img: "img/view-sky.jpg"
+    }
+  ];
